a480abf4ef0bf06d87d4b69ebc55e0ad0dc3d935
MishaKurghinyan1 <minecon291@gmail.com>
2026-06-23 10:47:09 +0400

    feat(components): add header and main components

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.css';
+export function Header() {
+  return (
+    <header className="header">
+      <h1>Async Race</h1>
+    </header>
+  );
+}
---
+++ src/Components/Main/Main.tsx
@@ -0,0 +1,9 @@
+import styles from './Main.module.css';
+
+export function Main() {
+  return (
+    <main className="main">
+      <h2>Async Race</h2>
+    </main>
+  );
+}
